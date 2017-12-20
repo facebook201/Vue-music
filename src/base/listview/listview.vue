70e38abedd0649db4c4894d075607446f6757667
syo <syolmm@qq.com>
2017-12-20 20:55:47 +0800

    listview

---
+++ src/base/listview/listview.vue
@@ -0,0 +1,114 @@
+<template>
+  <scroll class="listview" :data="data">
+    <ul>
+      <li v-for="group in data" :key="group.id" class="list-group">
+        <h2 class="list-group-title">{{group.title}}</h2>
+        <ul>
+          <li v-for="item in group.items" :key="item.id" class="list-group-item">
+            <img width="50" height="50" class="avatar" v-lazy="item.avatar" />
+            <span class="name">{{item.name}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+
+    <div class="list-shortcut">
+      <ul>
+        <li v-for="item in shortcutList" :key="item.id" class="item">
+          {{item}}
+        </li>
+      </ul>
+    </div>
+
+  </scroll>
+</template>
+
+<script type="text/ecmascript-6">
+import Scroll from 'base/scroll/scroll';
+
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: []
+    }
+  },
+  computed: {
+    shortcutList() {
+      return this.data.map((group) => {
+        return group.title;
+      });
+    }
+  },
+  components: {
+    Scroll
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~common/stylus/variable';
+
+.listview
+  position: relative
+  width: 100%
+  height: 100%
+  overflow: hidden
+  background: $color-background
+  .list-group
+    padding-bottom: 30px
+    .list-group-title
+      height: 30px
+      line-height: 30px
+      padding-left: 20px
+      font-size: $font-size-small
+      color: $color-text-l
+      background: $color-highlight-background
+    .list-group-item
+      display: flex;
+      align-items: center
+      padding: 20px 0 0 30px;
+      .avatar
+        display: block
+        border-radius: 50%
+      .name
+        margin-left: 20px
+        color: $color-text-l
+        font-size: $font-size-medium
+  .list-shortcut
+    position: absolute
+    z-index: 30
+    right: 0
+    top: 50%
+    transform: translateY(-50%)
+    width: 20px
+    padding: 20px 0
+    border-radius: 10px
+    text-align: center
+    background: $color-background-d
+    font-size: $font-size-small
+    .item
+      padding: 3px
+      line-height: 1
+      color: $color-text-l
+      font-size: $font-size-samll
+    &.current
+      color: $color-them
+  .list-fixed
+    position: absolute
+    top: 0
+    left: 0
+    width: 100%
+    .fixed-title
+      height: 30px
+      line-height: 30px
+      padding-left: 30px
+      font-size: $font-size-small
+      color: $color-text-l
+      background: $color-highlight-background
+  .loading-container
+    position: absolute
+    width: 100%
+    top: 50%
+    transform: translateY(-50%)      
+</style>
--- src/component/singer/singer.vue
+++ src/component/singer/singer.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="singer">
+    <list-view :data="singers"></list-view>
   </div>
 </template>
 
 <script>
 import { getSingerList } from 'api/singer';
 import { ERR_OK } from 'api/config';
+import Singer from 'common/js/singer';
+import ListView from 'base/listview/listview';
+
+const HOT_NAME = '热门';
+const HOT_SINGER_LEN = 10;
 
 export default {
   data() {
     return {
       singers: []
     };
+  },
+  components: {
+    ListView
   },
   created() {
     this._getSingerList();
@@ -20,10 +29,56 @@ export default {
     _getSingerList() {
       getSingerList().then(res => {
         if (res.code === ERR_OK) {
-          this.singers = res.data.list;
-          console.log(this.singers);
+          this.singers = this._normalizeSinger(res.data.list);
         }
       });
+    },
+    // 处理返回的数据 因为有热门数据
+    _normalizeSinger(list) {
+      let map = {
+        // 热门数据
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      };
+      list.forEach((item, index) => {
+        // 如果索引小于常量
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }));
+        }
+        const key = item.Findex;
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          };
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }));
+      });
+
+      // 为了得到有序的列表 处理map
+      let hot = [];
+      let ret = [];
+
+      for (let k in map) {
+        let val = map[k];
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val);
+        } else if (val.title === HOT_NAME) {
+          hot.push(val);
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+      });
+      return hot.concat(ret);
     }
   }
 };
